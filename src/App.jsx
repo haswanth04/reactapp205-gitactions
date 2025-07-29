9db29791b67766581eade177bc4d153d77bedd7f
haswanth <haswnth.chinni@gmail.com>
2025-07-29 12:06:32 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className="container">
       <h1>Welcome to My React Project - HASWANTH</h1>
       <p>CI/CD and cloud DevOps</p>
-      <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
+      <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
     </div>
   );
 }
